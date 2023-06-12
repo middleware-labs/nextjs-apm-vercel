e1c49cca285b834862fea2ba3a7047f32e11547b
jagrut91solanki <jagrutsolanki1@gmail.com>
2023-06-12 13:00:12 +0530

    instrumentation file added for vercel MW nextjs apm and logs

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,10 +1,17 @@
 // @ts-ignore
-import { track } from '@middleware.io/agent-apm-nextjs';
+import tracker from '@middleware.io/agent-apm-nextjs';
 
 export function register() {
-    track({
+    tracker.track({
         projectName: "mw-project-vercel",
         serviceName: "mw-service-vercel",
+         accountKey: "erqorzulkqdjgdrptjcwrrzacohsousghiqk",
         target: "vercel",
     });
+     tracker.info("Info Sample");
+        tracker.warn("Warn Sample", {
+            "tester": "Jagrut",
+        });
+        tracker.debug("Debug Sample");
+        tracker.error("Error Sample");
 }
